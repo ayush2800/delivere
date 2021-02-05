1463452c4fb63cdd8eeaa6ce8357e2a6c8c567e2
ayush sharma <ayush@ayushs-MBP.lan>
2021-02-05 02:33:00 -0500

    basic working

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import homeSection from "./component/home" ;
 import 'bootstrap/dist/css/bootstrap.min.css';
-import aboutSection from "./component/about"
 
 
 
 function App() {
-  return (
+    return (
 
-  <body data-new-gr-c-s-check-loaded="14.993.0" data-gr-ext-installed="">
+      <body data-new-gr-c-s-check-loaded="14.993.0" data-gr-ext-installed="">
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">DeliverE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">DeliverE</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+        <div class="collapse navbar-collapse " id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">whaaat</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/send">Send</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/track">Track</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/contact">Contact</a>
+            </li>
+            
+          </ul>
 
-    <div class="collapse navbar-collapse " id="navbarsExampleDefault">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">Send</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">Track</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">Contact</a>
-        </li>
-        
-      </ul>
+        </div>
+      </nav>
 
-    </div>
-  </nav>
+      <main role="main" class="container">
+      
+      <aboutSection />
+      </main>
 
-  <main role="main" class="container">
-
-   <aboutSection />
-  </main>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"></script>')</script>
-  <script src="../../assets/js/vendor/popper.min.js"></script>
-  <script src="../../dist/js/bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"></script>')</script>
+      <script src="../../assets/js/vendor/popper.min.js"></script>
+      <script src="../../dist/js/bootstrap.min.js"></script>
 
 
-</body>
-  );
-}
+      </body>
+    );
+    }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
-
+var Router = require('react-router')
+var Route = Router.Route
+var Link = Router.Link
 
 
 ReactDOM.render(
